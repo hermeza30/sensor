979774ac94c504785bdef13b10280d08f123cb97
hermeza30 <hermez.antonio@gmail.com>
2020-08-13 10:02:13 -0300

    Socket io install

--- server/routes/sensor.js
+++ server/routes/sensor.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bcrypt = require("bcrypt");
+const { io } = require('../server');
 const app = express();
 
 
+io.on('connection',(client)=>{
+    console.log('cliente conectado');
+    client.on('disconnect',()=>{
+        console.log('cliente desconectado');
+
+    });
+});
 app.get('/sensor', (req, res) => {
     res.json('Conectados a sensor')
 });
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 require('./config/config');
 const express=require('express');
 const bodyparser=require('body-parser');
+const socketIO=require('socket.io');
+const http=require('http');
 const mongoose=require('mongoose');
 const app=express();
 const path=require('path');
-
+let server=http.createServer(app);
 //CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -16,7 +18,10 @@ app.use(function(req, res, next) {
 app.use(bodyparser.json());
 //carpeta publica
 app.use(express.static(path.resolve(__dirname+'/../public')));
-//Configuracion global de rutas
+// IO = esta es la comunicacion del backend
+module.exports.io = socketIO(server);
+
+
 app.use(require('./routes/index'));
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
@@ -25,6 +30,6 @@ mongoose.connect(process.env.URLDB,(err,res)=>{
     if(err)throw err;
     console.log("Base de datos ONLINE")
 });
-app.listen(process.env.PORT,()=>{
+server.listen(process.env.PORT,()=>{
     console.log("Escuchando puerto 3000")
 });
